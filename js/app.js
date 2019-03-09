11ee67ba663d8082ce5207bfafab8ab21c8293cc
gangan786 <215387011@qq.com>
2019-03-09 16:32:42 +0800

    完成nickname的展示修改

--- js/app.js
+++ js/app.js
@@ -3,12 +3,12 @@ window.app = {
 	/**
 	 * netty服务后端发布的url地址
 	 */
-	nettyServerUrl: 'ws://192.168.1.102:8088/ws',
+	nettyServerUrl: 'ws://192.168.1.104:8088/ws',
 	
 	/**
 	 * 后端服务发布的url地址
 	 */
-	serverUrl: 'http://192.168.1.102:8080',
+	serverUrl: 'http://192.168.1.104:8080',
 	
 	/**
 	 * 图片服务器的url地址
